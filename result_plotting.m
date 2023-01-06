a6e1207bc5d14b8b1dd8894b1461295a700b9163
Marte Konstanse Torgersen <martekonstanset@hotmail.com>
2023-01-06 10:55:26 +0100

    add task35 photos, plotting not working

--- result_plotting.m
+++ result_plotting.m
@@ -1,1 +1,17 @@
-filename = "sw_0037004_TUB614_0501.mat";
+global plot_colors
+
+folder = "C:\Users\marte\OneDrive\Documents\MATLAB\FlightControlWS22\FC-HW1";
+filename = fullfile(folder,"sw_00370004_TUB614_0501.mat");
+file_rp = load(filename);
+
+time_rp = file_rp.data.wd_time;
+data_rp = [file_rp.data.wd_rb; file_rp.data.wd_beta; file_rp.data.wd_pb; file_rp.data.wd_phi];
+
+label_y = ["$r [^\circ/s]$","$\beta [^\circ]$","$p [^\circ/s]$","$\Phi [^\circ]$"];
+ones_array = ones([1,26442])*0.5;
+figure(10); clf
+for i = 1:4
+    subplot(4,1,i); hold all; grid on; 
+    ylabel(label_y(i),'Interpreter','latex','FontSize',12);
+    plot(time_rp,ones_array,'LineWidth',1.5,'Color',plot_colors(1,:),'LineStyle','--');
+end
